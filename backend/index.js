18140135c057d2ad591215936e7e705f11d560f5
Ananda Kumar <anandakumar@Anandas-MacBook-Air.local>
2024-08-09 17:48:48 +0530

    new add

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,12 @@ app.use(express.json());
 
 app.use(cors());
 
-// app.use(cors({
-//     origin: ["https://book-store-front-ten.vercel.app"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true
-// })
-// );
+app.use(cors({
+    origin: ["https://book-store-front-ten.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+})
+);
 
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "https://book-store-front-ten.vercel.app");
