a213250a9635867a87d1a283847a966217b81dcb
Ananda Kumar <anandakumar@Anandas-MacBook-Air.local>
2024-08-09 19:55:58 +0530

    remove

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,6 @@ app.use(cors({
 })
 );
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://book-store-front-ten.vercel.app");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
-
 app.get("/", (req, res) => {
     console.log(req);
     return res.send("Hello!");
