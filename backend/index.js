64818df1fab859b974216ddac9d09e4bd9339ab7
Ananda Kumar <anandakumar@Anandas-MacBook-Air.local>
2024-08-09 16:01:48 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,14 @@ app.use(cors({
 })
 );
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+
 app.get("/", (req, res) => {
     console.log(req);
     return res.send("Hello!");
