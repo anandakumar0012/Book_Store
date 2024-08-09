027e3d686faa370fc230ae061dd33c808daae4c9
Ananda Kumar <anandakumar@Anandas-MacBook-Air.local>
2024-08-09 11:41:17 +0530

    update index file

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,12 @@ app.use(express.json());
 
 app.use(cors());
 
-// app.use(cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["content-Type"],
-// })
-// );
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true
+})
+);
 
 app.get("/", (req, res) => {
     console.log(req);
