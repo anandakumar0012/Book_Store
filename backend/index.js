18e03084aef8c2821da7dd710426ef8eccae74d0
Ananda Kumar <anandakumar@Anandas-MacBook-Air.local>
2024-08-09 19:30:23 +0530

    old method

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,14 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
-app.use(cors({
-    origin: ["https://book-store-front-ten.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-})
-);
+// app.use(cors({
+//     origin: ["https://book-store-front-ten.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+// })
+// );
 
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "https://book-store-front-ten.vercel.app");
